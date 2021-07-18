8a175d22e57301bd524e3554e5b901ce5f64532b
kay019 <crowruya@gmail.com>
2021-07-18 22:06:38 +0900

    Standard of Java
    01강 ~ 10강

---
+++ StandardOfJava/src/ch02/VarEx01.java
@@ -0,0 +1,19 @@
+package ch02;
+
+public class VarEx01 {
+    public static void main(String[] args){
+
+        System.out.println("6 + 3");
+        System.out.println(6 + 3);
+        System.out.println(6 - 3);
+        System.out.println(6 * 3);
+        System.out.println(6 / 3);
+
+        int x = 4, y = 2;
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+
+    }
+}
---
+++ StandardOfJava/src/ch02/ex02.java
@@ -0,0 +1,20 @@
+package ch02;
+
+public class ex02 {
+    public static void main(String[] args){
+
+        System.out.println("System.out.print");
+        System.out.print("print");
+        System.out.print("print");
+        System.out.println("System.out.println");
+        System.out.println("println");
+        System.out.println("println");
+        System.out.println("사칙연산");
+        System.out.println("5 + 3 = " + (5 + 3));
+        System.out.println("5 - 3 = " + (5 - 3));
+        System.out.println("5 * 3 = " + (5 * 3));
+        System.out.println("5 / 3 = " + (5 / 3.0));
+        System.out.println("5 % 3 = " + (5 % 3.0));
+
+    }
+}
